264316306391ef4cca786c0265309a603bf9ee48
Howard <howardthomas13@gmail.com>
2019-09-04 16:00:52 -0400

    Rename methods to PEP8 and added comments

--- Game.py
+++ Game.py
@@ -1,11 +1,11 @@
 import random
-
+# * Variables 
 board = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
 player_piece = ' '
 computer_piece = ' '
 playing = True
 
-
+# * display_board method which uses the board, amount of loops and rows to create a tic tac toe board
 def display_board(start_board, loops, rows):
     while loops < rows:
         if loops >= 1:
@@ -15,12 +15,12 @@ def display_board(start_board, loops, rows):
         print(' -------------')
         loops += 1
 
-
+#* Simple heading to start the game
 def heading():
     print("Tic-Tac-Toe")
     print('By: Howard Thomas')
 
-
+# * menu method to allow user to play and exit game as well as choose the piece they want to play with
 def menu():
     global player_piece
     user_input = input("Do you want to play? (yes or no) ")
@@ -28,7 +28,7 @@ def menu():
         print('Lets get started')
         player_piece = input(
             'What letter would you like to be? (X or O) ').upper()
-        setGamePiece(player_piece)
+        set_game_piece(player_piece)
     elif user_input.lower() == 'no' or 'n' or 'exit':
         print('Thanks for playing')
     else:
@@ -36,48 +36,48 @@ def menu():
         print('Hint: Use y or yes to play and no or n to exit')
         menu()
 
-
-def setGamePiece(game_piece):
+# * Sets game piece for the user and computer player
+def set_game_piece(game_piece):
     global computer_piece
     if game_piece.upper() == 'X':
         computer_piece = 'O'
     elif game_piece.upper() == 'O':
         computer_piece = 'X'
 
-
+# * game method/loop to play through the game
 def game():
     global playing
     counter = random.randint(1, 2)
     while playing:
         if counter == 1:
-            userMove(player_piece, board)
-            if winningCheck(board, player_piece):
+            user_move(player_piece, board)
+            if winning_check(board, player_piece):
                 print('You have won')
                 playing = False
                 display_board(board, 0, 5)
             else:
-                if not boardCheck(board):
+                if not board_check(board):
                     display_board(board, 0, 5)
                     print('Game is a tie')
                     playing = False
                 else:
                     counter = 2
         elif counter == 2:
-            computerMove(computer_piece, board, player_piece)
-            if winningCheck(board, computer_piece):
+            computer_move(computer_piece, board, player_piece)
+            if winning_check(board, computer_piece):
                 print('The Computer has won')
                 display_board(board, 0, 5)
                 playing = False
             else:
-                if not boardCheck(board):
+                if not board_check(board):
                     display_board(board, 0, 5)
                     print('Game is a tie')
                     playing = False
                 else:
                     counter = 1
 
-
-def userMove(game_piece, game_board):
+# * Allows user to move piece to valid spot on the board
+def user_move(game_piece, game_board):
     try:
         move = int(input('Where would you like to place you piece? '))
         if 0 < move < 11:
@@ -88,45 +88,45 @@ def userMove(game_piece, game_board):
             print('The number input was not a valid number')
     except:
         print('Please Type a Number')
-        userMove(player_piece, board)
+        user_move(player_piece, board)
 
-
-def computerMove(comp_piece, game_board, user_piece):
+# * Is the computers move 
+def computer_move(comp_piece, game_board, user_piece):
     computer_AI(game_board, comp_piece, user_piece)
     display_board(board, 0, 5)
-    winningCheck(board, comp_piece)
+    winning_check(board, comp_piece)
 
-
-def moveCheck(game_board, move_number):
+# * checks if move is valid
+def move_check(game_board, move_number):
     if game_board[move_number - 1] != player_piece and game_board[move_number - 1] != computer_piece:
         return True
     else:
         return False
 
-
-def doMove(game_board, current_piece, move_number):
+# * does the move if it is valid 
+def do_move(game_board, current_piece, move_number):
     game_board[move_number - 1] = current_piece
 
-
-def randomMove(board, movesList):
+# * allows computer to see possible moves
+def random_move(board, movesList):
     possibleMoves = []
     for i in movesList:
-        if moveCheck(board, i):
+        if move_check(board, i):
             possibleMoves.append(i)
     if len(possibleMoves) != 0:
         return random.choice(possibleMoves)
     else:
         return None
 
-
-def boardCheck(game_board):
+# *Check to see if their is any possible moves left to make. if none game is a tie 
+def board_check(game_board):
     for i in range(0, 10):
-        if moveCheck(board, i):
+        if move_check(board, i):
             return True
     return False
 
-
-def winningCheck(game_board, current_player):
+# * Check to see if anyone wins
+def winning_check(game_board, current_player):
     return ((game_board[0] == current_player and game_board[1] == current_player and game_board[2] == current_player) or
             (game_board[3] == current_player and game_board[4] == current_player and game_board[5] == current_player) or
             (game_board[6] == current_player and game_board[7] == current_player and game_board[8] == current_player) or
@@ -136,48 +136,48 @@ def winningCheck(game_board, current_player):
             (game_board[0] == current_player and game_board[4] == current_player and game_board[8] == current_player) or
             (game_board[2] == current_player and game_board[4] == current_player and game_board[6] == current_player))
 
-
+# * Computer AI 
 def computer_AI(game_board, comp_piece, user_piece):
     move = computer_win(game_board, comp_piece)
     if move != None:
-        doMove(game_board, comp_piece, move)
+        do_move(game_board, comp_piece, move)
     else:
-        move = computer_playerWinCheck(game_board, user_piece)
+        move = computer_player_win_check(game_board, user_piece)
         if move != None:
-            doMove(game_board, comp_piece, move)
+            do_move(game_board, comp_piece, move)
         else:
-            move = randomMove(game_board, [5])
+            move = random_move(game_board, [5])
             if move != None:
-                doMove(game_board, comp_piece, move)
+                do_move(game_board, comp_piece, move)
             else:
-                move = randomMove(game_board, [1, 3, 7, 9])
+                move = random_move(game_board, [1, 3, 7, 9])
                 if move != None:
-                    doMove(game_board, comp_piece, move)
+                    do_move(game_board, comp_piece, move)
                 else:
-                    move = randomMove(game_board, [2, 4, 6, 8])
+                    move = random_move(game_board, [2, 4, 6, 8])
                     if move != None:
-                        doMove(game_board, comp_piece, move)
+                        do_move(game_board, comp_piece, move)
 
-
-def computer_playerWinCheck(game_board, user_piece):
+# * Checks to see if player will win 
+def computer_player_win_check(game_board, user_piece):
     for i in range(1, 10):
-        copy = boardCopy(game_board)
-        if moveCheck(copy, i):
-            doMove(copy, user_piece, i)
-            if winningCheck(copy, user_piece):
+        copy = board_copy(game_board)
+        if move_check(copy, i):
+            do_move(copy, user_piece, i)
+            if winning_check(copy, user_piece):
                 return i
 
 
 def computer_win(game_board, comp_piece):
     for i in range(1, 10):
-        copy = boardCopy(game_board)
-        if moveCheck(copy, i):
-            doMove(copy, comp_piece, i)
-            if winningCheck(copy, comp_piece):
+        copy = board_copy(game_board)
+        if move_check(copy, i):
+            do_move(copy, comp_piece, i)
+            if winning_check(copy, comp_piece):
                 return i
 
-
-def boardCopy(game_board):
+# * Copy's board 
+def board_copy(game_board):
     copy_board = []
     for i in game_board:
         copy_board.append(i)
